60799ceb3879b97e37b1d2acfa8aa676fdac55be
federico-ardila <federico.ardila@gmail.com>
2013-11-18 17:44:47 +0100

    Commiting the Template without changes

---
+++ MVC5Test/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC5Test.Startup))]
+namespace MVC5Test
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
